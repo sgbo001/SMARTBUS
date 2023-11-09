0ab4b546588eb2b586bbec1b74d6037208010d19
Adetoyese Olaide <82801465+sgbo001@users.noreply.github.com>
2023-11-09 20:18:31 +0000

    Update settings.py

--- busApp/settings.py
+++ busApp/settings.py
@@ -20,12 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5crx$2#@-^fcme(fn!*#%ra@3^v!qb+0n40*a-af&(w499f&i3'
-
+#SECRET_KEY = 'django-insecure-5crx$2#@-^fcme(fn!*#%ra@3^v!qb+0n40*a-af&(w499f&i3'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your_current_secret_key_value')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+WEBSITE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME', None)
 
-ALLOWED_HOSTS = []
+DEBUG = WEBSITE_HOSTNAME == None
+
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = [WEBSITE_HOSTNAME]
+    CSRF_TRUSTED_ORIGINS = [f'https://{WEBSITE_HOSTNAME}']
 
 
 # Application definition
